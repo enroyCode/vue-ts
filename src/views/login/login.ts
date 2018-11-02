dec945d7690b41ba4b55defd81be4208a898f894
zhuchao <zhuchao@hd123.com>
2018-11-02 18:19:15 +0800

    初始化

---
+++ src/views/login/login.ts
@@ -0,0 +1,11 @@
+import { Component, Vue } from 'vue-property-decorator'
+import '../styles/index.scss';
+
+@Component({})
+export default class Login extends Vue {
+
+
+  mounted() {
+    console.log(1111);
+  }
+}
---
+++ src/views/login/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    <el-form>
+      <el-form-item>
+        <el-input v-model="name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="pwd"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts" src="login.ts"></script>
+<style lang="scss">
+  .login {
+
+  }
+</style>
