ee10824dc075c975811533c96ce37cce072d778c
zhuchao <zhuchao@hd123.com>
2018-11-08 15:12:20 +0800

    路由配置。

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../views/login/login.vue'
+import Home from '../views/home/home.vue'
+import ZcFrame from '../components/frame/zc-frame.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/login',
       name: 'Login',
       component: Login
+    }, {
+      path: '/',
+      component: ZcFrame,
+      children: [
+        {
+          path: '/',
+          name: 'Home',
+          component: resolve => require(['../views/home/home.vue'], resolve)
+        }]
     }
   ]
 })
---
+++ src/views/home/home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script lang="ts" src="./home.ts"></script>
+<style lang="scss">
+  .home {
+
+  }
+</style>
